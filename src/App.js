1bbe1dca71a4c8fa227b925332b4e0bcaf095b91
4bhishekKasam <konnurabhishek@gmail.com>
2018-06-21 21:00:58 +0530

    added new components

--- src/App.js
+++ src/App.js
@@ -2,20 +2,65 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const MyContext = React.createContext();
+
+class MyProvider extends Component {
+  state = {
+    firstName: 'Harry', lastName: 'Potter', age: 24,
+  }
+
+  render() {
+    return (
+      <MyContext.Provider value={{
+        state: this.state, growOld: () => this.setState({ age: this.state.age + 1 })
+      }}>
+
+        {this.props.children}
+
+      </MyContext.Provider>
+    );
+  }
+}
+
+class Family extends Component {
+  render() {
+    return (
+      <div>
+        <Person />
+      </div>
+    );
+  }
+}
+
+class Person extends Component {
+  render() {
+    return (
+      <div>
+        <MyContext.Consumer>
+          {(context) => (
+            <React.Fragment>
+              <p>Name : {context.state.firstName} {context.state.lastName} </p>
+              <p>Age : {context.state.age}</p>
+              <p><button onClick={context.growOld}>click me</button></p>
+            </React.Fragment>
+          )}
+        </MyContext.Consumer>
+      </div>
+    );
+  }
+}
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Reactjs</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="App-intro">
-          Hey guys
-        </p>
+        <MyProvider>
+          <div>
+            <p>I am in App component</p>
+            <Family />
+          </div>
+        </MyProvider>
       </div>
     );
   }
